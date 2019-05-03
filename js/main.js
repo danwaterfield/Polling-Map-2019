2a259a68cf3f93662139b63720be95e9615aa53b
Jacob Weinbren <jacobweinbren@gmail.com>
2019-05-03 15:38:10 +0100

    Tweaks to map choice

--- js/main.js
+++ js/main.js
@@ -21,15 +21,14 @@ $.ajax({
     success: function(data) {
         results = $.csv.toArrays(data);
     }
-});
+})
 
-
-$('.hex')[0].addEventListener('load', function() {
-    svg = $('.hex').getSVG();
-    constituencies = $(svg.find('g#constituencies path'));
+$('.map')[0].addEventListener('load', function() {
+    svg = $('.map').getSVG();
+    constituencies = $(svg.find('path'));
     $(constituencies).each(function(index) {
         element = $(constituencies[index]);
-        constituency = element.attr('constituency')
+        constituency = element.attr('title')
         for (i = 0; i < results.length; i++) {
             if (results[i][0] == constituency) {
                 element.css('fill', colours[results[i][1]])
@@ -37,44 +36,3 @@ $('.hex')[0].addEventListener('load', function() {
         }
     });
 });
-
-$('.geo')[0].addEventListener('load', function() {
-    svg = $('.geo').getSVG();
-    constituencies = $(svg.find('g#layer2 path'));
-    $(constituencies).each(function(index) {
-        element = $(constituencies[index]);
-        constituency = element.attr('constituency')
-
-        if (constituency) {
-            for (i = 0; i < results.length; i++) {
-                if (results[i][0] == constituency) {
-                    element.css('fill', colours[results[i][1]])
-                }
-            }
-        }
-    });
-});
-/*
-$('.svg')[0].addEventListener('load', function() {
-    svg = $('.geo').getSVG();
-    hex_orkney = svg.find("[constituency='Orkney and Shetland']").attr('d')
-
-    svg = $('.hex').getSVG();
-
-    geo_orkney = svg.find("[constituency='Orkney and Shetland']").attr('d')
-    var interpolator = flubber.interpolate(hex_orkney, geo_orkney);
-
-    requestAnimationFrame(draw);
-    svg = $('.geo').getSVG();
-    element = svg.find("[constituency='Orkney and Shetland']")
-
-    t = 0
-
-    function draw(time) {
-        t += 0.01
-        element.attr("d", interpolator(t));
-        if (t < 1) {
-            requestAnimationFrame(draw);
-        }
-    }
-});*/
